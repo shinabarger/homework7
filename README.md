08f49b9c7f0b15a3a0e355c30781663406b27aed
David Shinabarger <davidshinabarger@gmail.com>
2020-03-24 08:24:13 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# homework7
-Javascript homework for Web Design class at University of Michigan 
+# homework_7
+Starter Code for Homework7
---
+++ js/canvas.js
@@ -0,0 +1,15 @@
+// Variables!
+var color ="red";
+var radius = 15;
+//You will want to add more
+
+
+//Listeners!!
+//Add a listener for loading the window
+//Add a listener for the mouse movement
+//Add a listener for the touch move
+//Add a listener for the keydown
+
+
+// Functions!
+// I would add a function for draw
